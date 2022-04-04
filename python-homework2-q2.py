dc18879f9edbe8987677c8b0697ffaeeefc4a57d
KennyJHendon <98424107+KennyJHendon@users.noreply.github.com>
2022-04-04 15:51:06 -0400

    Add files via upload

---
+++ python-homework2-q2.py
@@ -0,0 +1,21 @@
+# Use the following list - [1,11,14,5,8,9]
+
+the_list = [1,14,11,5,8,9]
+
+
+def lessThanTen(a_list):
+    new_list = []
+    for num in a_list:
+        if num <= 10:
+            new_list.append(num)
+        else:
+            continue
+        
+    
+            
+        return(new_list)
+            
+   
+        
+
+lessThanTen(the_list)
