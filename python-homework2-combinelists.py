206b4915cdd9f7a5e3fe072966fb5fbbc587cb02
KennyJHendon <98424107+KennyJHendon@users.noreply.github.com>
2022-04-04 22:16:12 -0400

    Add files via upload

---
+++ python-homework2-combinelists.py
@@ -0,0 +1,13 @@
+list_1 = [1,2,3,4,5,6]
+list_2 = [3,4,5,6,7,8,10]
+
+def listCombine(first_list, second_list):
+
+    new_list = (first_list + second_list)
+    
+    new_list.sort
+    
+    return new_list
+    
+listCombine(list_1, list_2)
+
